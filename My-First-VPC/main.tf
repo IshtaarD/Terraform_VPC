49c559eb108ed32b0edd4e8ac95bca9d635908f6
Ishtaar <ishtaar.desravines@gmail.com>
2022-10-22 02:48:45 -0400

    added file for VPC

---
+++ My-First-VPC/main.tf
@@ -0,0 +1,77 @@
+# VARIABLES - can use variables below with a .tfvars file but configured an aws credentials file with the access/secret access keys. 
+# variable "aws_access_key" {}
+# variable "aws_secret_key" {}
+
+
+# PROVIDER
+provider "aws" {
+  #access_key = var.aws_access_key
+  #secret_key = var.aws_secret_key
+  region = "us-east-1"
+}
+
+# VPC
+resource "aws_vpc" "VPC" {
+  cidr_block           = "172.23.0.0/16"
+  enable_dns_hostnames = "true"
+
+  tags = {
+    "Name" : "Kura_VPC_1"
+  }
+}
+# ELASTIC IP
+resource "aws_eip" "NAT_EIP" {
+  vpc = true
+}
+# NAT GATEWAY
+resource "aws_nat_gateway" "NAT_GATE" {
+  allocation_id = aws_eip.NAT_EIP.id
+  subnet_id     = aws_subnet.public_subnet_1.id
+}
+# SUBNET 1
+resource "aws_subnet" "public_subnet_1" {
+  cidr_block              = "172.23.0.0/18"
+  vpc_id                  = aws_vpc.VPC.id
+  map_public_ip_on_launch = true
+  availability_zone       = data.aws_availability_zones.available.names[0]
+}
+# SUBNET 2
+resource "aws_subnet" "private_subnet_1" {
+  cidr_block              = "172.23.64.0/18"
+  vpc_id                  = aws_vpc.VPC.id
+  map_public_ip_on_launch = false
+  availability_zone       = data.aws_availability_zones.available.names[0]
+}
+# INTERNET GATEWAY
+resource "aws_internet_gateway" "" {
+  vpc_id = aws_vpc.VPC.id
+}
+# ROUTE TABLE
+resource "aws_route_table" "route_table" {
+  vpc_id = aws_vpc.VPC.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.NAT_GATE.id
+  }
+}
+resource "aws_route_table_association" "" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.route_table.id
+}
+# PRIVATE ROUTE TABLE
+resource "aws_route_table" "route_table_private" {
+  vpc_id = aws_vpc.VPC.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.NAT_GATE.id
+  }
+}
+resource "aws_route_table_association" "" {
+  subnet_id      = aws_subnet.private_subnet_1.id
+  route_table_id = aws_route_table.route_table_private.id
+}
+
+# DATA
+data "aws_availability_zones" "available" {
+  state = "available"
+}
